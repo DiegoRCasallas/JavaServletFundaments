bb067dcf962555bf941b6ebef735cb6b13c2b38f
Diego Casallas <master.drcd@gmail.com>
2023-05-29 17:01:49 -0500

    Creacion del primer serverlet con Tomcat

---
+++ src/main/java/com/alura/gerenciador/servlet/HolaMundoServlet.java
@@ -0,0 +1,28 @@
+package com.alura.gerenciador.servlet;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.annotation.WebServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = "/hola")
+public class HolaMundoServlet extends HttpServlet {
+
+    /*Enviamos y obtenemos rspuesta al navegador*/
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+        PrintWriter out= resp.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<H1>Felicitaciones por crear tu primer Servlet</H1>");
+        out.println("</body>");
+        out.println("</html>");
+
+        System.out.println("SE EJECUTO EL SERVLET");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello World! Desde Intellij</h2>
+</body>
+</html>
